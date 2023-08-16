4b2f7ffb5a11f9346f89149c8cd2ec3214a36df1
Sojeong <Sojeong@DESKTOP-52JJSB7>
2023-08-17 01:36:11 +0900

    Prac01_end_0817

--- src/section05/HomewordPrac02.java
+++ src/section05/HomewordPrac02.java
@@ -19,31 +19,80 @@ public class HomewordPrac02 {
 		
 		// 숙제 2 (트리) 조건으로 연습
 		
-		for (int i = 0; i < 4; i++) {
+//		for (int i = 0; i < 4; i++) {
+//			
+//			if (4 - i > 1) {
+//				for (int j = 2; j - i >=0; j--) {
+//					System.out.print(" ");		
+//				}
+//			}
+//			
+//			if (i < 4) {
+//				for (int k = 0; k < i + 1; k++) {
+//					System.out.print("*");
+//				}
+//				
+//			}
+//			if (i > 0) {
+//				for (int l = 1; l < i+1; l++) {
+//					System.out.print("*");
+//				}
+//			}
+//			
+//			System.out.println();
 			
-			if (4 - i > 1) {
-				for (int j = 2; j - i >=0; j--) {
+ // 여기까지 트리 완성 
+		
+		// 트리 간략화 
+//		for (int i = 0; i < 4; i++) {
+//			
+//			if (i < 3) {
+//				for (int j = 2; j - i >=0; j--) {
+//					System.out.print(" ");		
+//				}
+//			}
+//			
+//			if (i < 4) {
+//				for (int k = 0; k < (2*i)+1 ; k++) {
+//					System.out.print("*");
+//				}
+//			}
+//		
+//			System.out.println();
+		
+		// 11줄 다이아몬드
+		
+		for (int i = 0; i < 11; i++) {
+			
+			if (i < 5) {
+				for (int j = 4; j - i >= 0; j--) {
 					System.out.print(" ");		
 				}
 			}
+			if (i > 5) {
+				for (int l = 0; l < (i%6)+1; l++) {
+					System.out.print(" ");
+				}
+			}
 			
-			if (i < 4) {
-				for (int k = 0; k < i + 1; k++) {
+			if (i < 11) {
+				if (i < 6) {
+					for (int k = 0; k < (2*i)+1; k++) {
 					System.out.print("*");
+				}
+				}
+				else if (i >= 6) {
+					for (int m = 8; m-2*(i%6) >= 0; m--) {
+						System.out.print("*");
+					}
+					}
 				}
 				
-			}
-			if (i > 0) {
-				for (int l = 1; l < i+1; l++) {
-					System.out.print("*");
-				}
-			}
-			
 			System.out.println();
 			
-		} // 여기까지 트리 완성 
-		
 
+			
+		} 
 		
 	}
 
--- src/section05/HomeworkPrac03.java
+++ src/section05/HomeworkPrac03.java
@@ -3,32 +3,39 @@ package section05;
 public class HomeworkPrac03 {
 	public static void main(String[] args) {
 		
-		//다이아몬드 찍기
+		//다이아몬드 찍기(간략화)
+		// i가 0부터 시작하는 거 아닐때는 (i%시작하는숫자)로 해줘야 0, 1, 2... 되는듯?
 		
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 7; i++) {
 			
-			if (4 - i > 1) {
-				for (int j = 2; j - i >=0; j--) {
+			if (i < 3) {
+				for (int j = 2; j - i >= 0; j--) {
 					System.out.print(" ");		
 				}
 			}
+			if (i > 3) {
+				for (int l = 0; l < (i%4)+1; l++) {
+					System.out.print(" ");
+				}
+			}		
 			
-			if (i < 4) {
-				for (int k = 0; k < i + 1; k++) {
+			if (i < 7) {
+				if (i < 4) {
+					for (int k = 0; k < (2*i)+1; k++) {
 					System.out.print("*");
+				}
+				}
+				else if (i >= 4) {
+					for (int m = 4; m-2*(i%4) >= 0; m--) {
+						System.out.print("*");
+					}
+					}
 				}
 				
-			}
-			if (i > 0) {
-				for (int l = 1; l < i+1; l++) {
-					System.out.print("*");
-				}
-			}
-			
 			System.out.println();
 			
-		} // 여기까지 트리 완성 
-		
+		} 
+			
 	}
 
 }
